b74ac760f8a71ef9b922494b44a59a970e76bf91
Tushar365 <tusharthokdar10@gmail.com>
2025-02-28 00:11:23 +0530

    initial

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,5 @@
-import tailwindcss from '@tailwindcss/postcss'; // Import from @tailwindcss/postcss
-import autoprefixer from 'autoprefixer';
-
 const config = {
-  plugins: [
-    tailwindcss({}), // Use the imported tailwindcss from @tailwindcss/postcss
-    autoprefixer({}),
-  ],
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
